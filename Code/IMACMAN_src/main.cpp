4d58995d44bc0624acd79ceb8f0cf39e389f982d
Davdouv <d4vid.n@hotmail.fr>
2017-12-20 15:53:12 +0100

    Window Ratio & Main 3D Matrix

---
+++ Code/IMACMAN_src/main.cpp
@@ -0,0 +1,55 @@
+#include <glimac/SDLWindowManager.hpp>
+#include <GL/glew.h>
+#include <iostream>
+
+using namespace glimac;
+
+int main(int argc, char** argv) {
+    // Initialize SDL and open a window
+    SDLWindowManager windowManager(1280, 720, "GLImac");
+
+    // Initialize glew for OpenGL3+ support
+    GLenum glewInitError = glewInit();
+    if(GLEW_OK != glewInitError) {
+        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Enable GPU depth test for 3D rendering
+    glEnable(GL_DEPTH_TEST);
+
+    // Matrix declaration
+    glm::mat4 ProjMatrix, MVMatrix, NormalMatrix;
+
+    // Projection Matrix (world) : vertical view angle, window ratio, near, far
+    ProjMatrix = glm::perspective(glm::radians(70.f), windowManager.getRatio(), 0.1f, 100.f);
+    // ModelView Matrix (camera)
+    MVMatrix = glm::translate((1.f), glm::vec3(0.0f, 0.0f, -5.f));
+    // Normal Matrix in the camera landmark
+    NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+
+    /*********************************
+     * HERE SHOULD COME THE INITIALIZATION CODE
+     *********************************/
+
+    // Application loop:
+    bool done = false;
+    while(!done) {
+        // Event loop:
+        SDL_Event e;
+        while(windowManager.pollEvent(e)) {
+            if(e.type == SDL_QUIT) {
+                done = true; // Leave the loop after this iteration
+            }
+        }
+
+        /*********************************
+         * HERE SHOULD COME THE RENDERING CODE
+         *********************************/
+
+        // Update the display
+        windowManager.swapBuffers();
+    }
+
+    return EXIT_SUCCESS;
+}
