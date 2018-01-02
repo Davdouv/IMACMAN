b5b0827c86bce27494aab4b2bf9bca423ce6b266
Davdouv <d4vid.n@hotmail.fr>
2018-01-02 15:07:36 +0100

    Main is now more clear

--- Code/IMACMAN_src/main.cpp
+++ Code/IMACMAN_src/main.cpp
@@ -20,6 +20,9 @@ using namespace glimac;
 
 int main(int argc, char** argv) {
 
+    /* -------------
+    *   INIT WINDOW
+    *  ------------- */
 
     // Default window size
     glm::vec2 defaultWindowSize = glm::vec2(1280,720);
@@ -33,6 +36,13 @@ int main(int argc, char** argv) {
         std::cerr << glewGetErrorString(glewInitError) << std::endl;
         return EXIT_FAILURE;
     }
+
+    // Enable GPU depth test for 3D rendering
+    glEnable(GL_DEPTH_TEST);
+
+    /* -------------
+    *   INIT PROGRAMS
+    *  ------------- */
 
     // Create Programs (1 fragment shader = 1 program)
     FilePath applicationPath(argv[0]);
@@ -49,20 +59,14 @@ int main(int argc, char** argv) {
     programList.directionnalLightProgram = &directionnalLightProgram;
     programList.pointLightProgram = &pointLightProgram;
 
-    // Enable GPU depth test for 3D rendering
-    glEnable(GL_DEPTH_TEST);
-
-    /*********************************
-     * HERE SHOULD COME THE INITIALIZATION CODE
-     *********************************/
+    /* --------------------------------------------
+    *   INIT GAME | CAMERAS | CONTROLLER | RENDERER
+    *  -------------------------------------------- */
 
     Map map;
     map.setFileMap("classicMap.txt");
-    //map.setFileMap("mapTest.txt");
     map.initialization();
-
     GameManager gameManager = GameManager(&map);
-
     // Game Infos
     glm::vec2 gameSize = glm::vec2(map.getNbX(),map.getNbY());
 
@@ -76,24 +80,32 @@ int main(int argc, char** argv) {
 
     // Load Textures
     renderManager.loadTextures();
-
     // initialize Skybox
     renderManager.initSkybox();
-
     // initialize the timers
     gameManager.setTimers();
-
+    // Time & Delta Time
     windowManager.updateDeltaTime();
     gameManager.setStartTime(SDL_GetTicks());
 
-    // Application loop:a
+    /* ------------------------------------------------------------
+    *   APPLICATION LOOP | 1.EVENTS | 2.GAME ENGINE | 3. RENDERING
+    *  ------------------------------------------------------------ */
+    
     bool done = false;
     while(!done) {
 
+        /* ------------------
+        *   UPDATE DELA TIME
+        *  ------------------ */
+        
         windowManager.updateDeltaTime();
         gameManager.updateSpeed(windowManager.getDeltaTime());
 
-        // Event loop:
+        /* ------------------
+        *   EVENT LOOP
+        *  ------------------ */
+
         SDL_Event e;
         while(windowManager.pollEvent(e)) {
             if(e.type == SDL_QUIT)
@@ -105,12 +117,15 @@ int main(int argc, char** argv) {
             controller.updateController(map.getPacman());
         }
 
+        /* ------------------
+        *   MANAGE CAMERAS
+        *  ------------------ */
+
         // Send the keys to the camera and the map
         tpsCamera.cameraController(&controller);
-        fpsCamera.setCameraOnCharacter(map.getPacman(), gameSize);     // NEED TO FIX HERE !!
-        gameManager.play(&controller);
-
-        // Switch Camera mini-function
+        // Ask the camera to track pacman
+        fpsCamera.setCameraOnCharacter(map.getPacman(), gameSize);
+        // Switch Camera FPS / TPS if C button is pressed
         if (controller.getInterfaceAction() == Controller::C)
         {
             if(camera == &fpsCamera)
@@ -126,23 +141,26 @@ int main(int argc, char** argv) {
             controller.setInterfaceAction(Controller::NONE);
         }
 
-        /*********************************
-         * HERE SHOULD COME THE RENDERING CODE
-         *********************************/
+        /* --------------------------------------------------------------------------
+        *   PLAY FUNCTION : Move characters, Check for collision, Update Player infos
+        *  -------------------------------------------------------------------------- */
+
+        gameManager.play(&controller);
+
+        /* ------------------
+        *   RENDERING CODE
+        *  ------------------ */
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
         // Update The View Matrix each time we enter the while loop
         renderManager.updateMVMatrix(camera, map.getPacman());
+        // Render the map (objects, skybox and ground)
         renderManager.drawMap(&map, &controller);
 
         // Update the display
         windowManager.swapBuffers();
     }
-
-    // FREE RESSOURCES
-    // See ~renderManager destructor
 
     return EXIT_SUCCESS;
 
