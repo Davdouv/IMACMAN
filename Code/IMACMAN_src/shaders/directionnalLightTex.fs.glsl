86a405960ff0b7373478bf87be71f4283fdcf874
Davdouv <d4vid.n@hotmail.fr>
2018-01-11 15:17:59 +0100

    Fix Shader

--- Code/IMACMAN_src/shaders/directionnalLightTex.fs.glsl
+++ Code/IMACMAN_src/shaders/directionnalLightTex.fs.glsl
@@ -42,6 +42,6 @@ vec3 blinnPhong(vec3 Kd, vec3 Ks, float shininess, vec3 lightDirection, vec3 Li,
 void main() {
     vec4 textureColor = texture(uTexture, vFragTexCoords);
     //fFragColor = blinnPhong(uKd, uKs, uShininess, uLightDir_vs, uLightIntensity, normalize(vFragNormal))*vec3(textureColor);
-    //fFragColor = blinnPhong(uKd, uKs, uShininess, uLightDir_vs, uLightIntensity, normalize(vFragNormal)) * vec3(textureColor) * uColor;
-    fFragColor = vFragNormal;
+    fFragColor = blinnPhong(uKd, uKs, uShininess, uLightDir_vs, uLightIntensity, normalize(vFragNormal)) * vec3(textureColor) * uColor;
+    //fFragColor = vFragNormal;
 }
